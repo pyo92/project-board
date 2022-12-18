27170779e966ba0eece8406de539657784246297
PARK GI-PYO <gipyopark@gmail.com>
2022-12-18 21:40:46 +0900

    #9 - Entity, JPA Repository 설정
    - Entity (Article, ArticleComment)
    - JPA Repository (Article, ArticleComment)
    - JPA Auditing

---
+++ src/main/java/com/example/projectboard/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.example.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        //TODO: Spring Security 인증 기능 추가 시, 수정
+        return () -> Optional.of("gp");
+    }
+
+}
--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -1,15 +1,87 @@
 package com.example.projectboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashTag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+
+    @Setter
     private String hashTag;
 
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private List<ArticleComment> articleComments = new ArrayList<>();
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashTag) {
+        this.title = title;
+        this.content = content;
+        this.hashTag = hashTag;
+    }
+
+    public static Article of(String title, String content, String hashTag) {
+        return new Article(title, content, hashTag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
