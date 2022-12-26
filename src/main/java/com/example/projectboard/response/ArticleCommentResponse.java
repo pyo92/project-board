5bf2384ec7807dcd4e8b56498b34e7596e21a7b9
PARK GI-PYO <gipyopark@gmail.com>
2022-12-27 04:16:08 +0900

    #22 - Response 클래스 작성
    - controller 단에서 객체를 받아서 사용할 response 클래스
    - 해당 클래스는 dto 를 기반으로 재생성하여 반환하도록 처리

---
+++ src/main/java/com/example/projectboard/response/ArticleCommentResponse.java
@@ -0,0 +1,34 @@
+package com.example.projectboard.response;
+
+import com.example.projectboard.dto.ArticleCommentDto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleCommentResponse(
+        Long id,
+        String content,
+        LocalDateTime createdAt,
+        String email,
+        String nickname
+) implements Serializable {
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    }
+
+    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+        String nickname = dto.memberDto().nickName();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.memberDto().userId();
+        }
+
+        return new ArticleCommentResponse(
+                dto.id(),
+                dto.content(),
+                dto.createdAt(),
+                dto.memberDto().email(),
+                nickname
+        );
+    }
+}
---
+++ src/main/java/com/example/projectboard/response/ArticleWithCommentResponse.java
@@ -0,0 +1,45 @@
+package com.example.projectboard.response;
+
+import com.example.projectboard.dto.ArticleWithCommentsDto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record ArticleWithCommentResponse(
+        Long id,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String email,
+        String nickname,
+        List<ArticleCommentResponse> articleCommentsResponse
+) implements Serializable {
+
+    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, List<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    }
+
+    public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
+        String nickname = dto.memberDto().nickName();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.memberDto().userId();
+        }
+
+        return new ArticleWithCommentResponse(
+                dto.id(),
+                dto.title(),
+                dto.content(),
+                dto.hashTag(),
+                dto.createdAt(),
+                dto.memberDto().email(),
+                nickname,
+                dto.articleCommentDtos().stream()
+                        .map(ArticleCommentResponse::from)
+                        .collect(Collectors.toCollection(LinkedList::new))
+        );
+    }
+}
