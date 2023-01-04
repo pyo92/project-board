410376808ccef0ed64226e6fdc6f08c460b4b3f3
PARK GI-PYO <gipyopark@gmail.com>
2023-01-04 11:40:31 +0900

    [feat] #37 : 댓글 저장, 삭제 기능을 위한 컨트롤러, 서비스 코드 작성

---
+++ src/test/java/com/example/projectboard/service/ArticleCommentServiceTest.java
@@ -0,0 +1,138 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.ArticleComment;
+import com.example.projectboard.domain.Member;
+import com.example.projectboard.dto.ArticleCommentDto;
+import com.example.projectboard.dto.MemberDto;
+import com.example.projectboard.repository.ArticleCommentRepository;
+import com.example.projectboard.repository.ArticleRepository;
+import com.example.projectboard.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+
+@DisplayName("비즈니스 로직 - 댓글")
+@ExtendWith(MockitoExtension.class)
+class ArticleCommentServiceTest {
+
+    @InjectMocks
+    private ArticleCommentService sut;
+
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @Mock
+    private ArticleCommentRepository articleCommentRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    ArticleCommentServiceTest() {
+    }
+
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(memberRepository.getReferenceById(dto.memberDto().id())).willReturn(createMember());
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
+
+        // When
+        sut.saveArticleComment(dto);
+
+        // Then
+        then(articleRepository).should().getReferenceById(dto.articleId());
+        then(memberRepository).should().getReferenceById(dto.memberDto().id());
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+
+    @DisplayName("댓글 저장을 시도했는데 맞는 게시글이 없으면, 경고 로그를 찍고 아무것도 안 한다.")
+    @Test
+    void givenNonexistentArticle_whenSavingArticleComment_thenLogsSituationAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.articleId())).willThrow(EntityNotFoundException.class);
+
+        // When
+        sut.saveArticleComment(dto);
+
+        // Then
+        then(articleRepository).should().getReferenceById(dto.articleId());
+        then(memberRepository).shouldHaveNoInteractions();
+        then(articleCommentRepository).shouldHaveNoInteractions();
+    }
+
+    @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
+    @Test
+    void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
+        // Given
+        Long articleCommentId = 1L;
+        willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
+
+        // When
+        sut.deleteArticleComment(articleCommentId);
+
+        // Then
+        then(articleCommentRepository).should().deleteById(articleCommentId);
+    }
+
+
+    private ArticleCommentDto createArticleCommentDto(String content) {
+        return ArticleCommentDto.of(
+                1L,
+                1L,
+                createMemberDto(),
+                content,
+                "pyo",
+                LocalDateTime.now(),
+                "pyo",
+                LocalDateTime.now()
+        );
+    }
+
+    private MemberDto createMemberDto() {
+        return MemberDto.of(
+                1L,
+                "pyo",
+                "pyo1234",
+                "gipyopark@gmail.com",
+                "pyo",
+                "I am pyo.",
+                "pyo",
+                LocalDateTime.now(),
+                "pyo",
+                LocalDateTime.now()
+        );
+    }
+
+    private Member createMember() {
+        return Member.of(
+                "pyo",
+                "pyo1234",
+                "giyopark@gmail.com",
+                "pyo",
+                "I am pyo."
+        );
+    }
+
+    private Article createArticle() {
+        return Article.of(
+                createMember(),
+                "title",
+                "content",
+                "#java"
+        );
+    }
+}
