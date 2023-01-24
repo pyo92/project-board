258e9dd0f797d40297ad598f11a82a4b9d1ed029
PARK GI-PYO <gipyopark@gmail.com>
2023-01-24 18:13:04 +0900

    [feat] #74 : 대댓글 비즈니스 로직 구현
    대댓글 관련 기능을 구현하면서 댓글 사이 부모-자식 관계가 생기고, 이에 따라 entity-dto 간 변환 로직, 대댓글을 저장하고 삭제하는 방법 등의 비즈니스 로직을 구현

--- src/main/java/com/example/projectboard/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/example/projectboard/dto/request/ArticleCommentRequest.java
@@ -6,14 +6,23 @@ import com.example.projectboard.dto.MemberDto;
 public record ArticleCommentRequest(
 
         Long articleId,
+        Long parentCommentId,
         String content
 ) {
 
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(MemberDto memberDto) {
-        return ArticleCommentDto.of(articleId, memberDto, content);
+        return ArticleCommentDto.of(
+                articleId,
+                memberDto,
+                parentCommentId,
+                content);
     }
 }
